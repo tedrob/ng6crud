4e4514ba8b83b45598b1913c27133be61ef8abed
Ted Robinson <tfqbtd@aol.com>
2018-06-15 16:20:35 -0500

    updated for  my-lib

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const express = require('express'),
       pg      = require('pg');
 
 express()
-  .use(express.static(path.join(__dirname, 'src')))
+  .use(express.static(path.join(__dirname, './dist/ng6crud/')))
+  .use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  })
+  .get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '\./dist/ng6crud/index.html'));
+  })
   .listen(port, () => {
     console.log(`listening on ${ port}`);
 

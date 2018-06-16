bb9fa9827c172b164b9cde724f6973f69dc4bb03
Ted Robinson <tfqbtd@aol.com>
2018-06-16 11:00:00 -0500

    moved bootstrap

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 const express = require('express'),
+      app     = express(),
       path    = require('path'),
-      port    = process.env.Port || 5000,
+      cors    = require('cors'),
+      port    = process.env.Port || 8080,
       bodyParser  = require('body-parser'),
       pg      = require('pg');
 
-express()
-  .use(express.static(path.join(__dirname, '\./dist/ng6crud/favicon.ico')))
-  /* .use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  }) */
+express((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header( 'Access-Control-Allow-Headers',
+              'Origin, X-Requested-With, Content-type,'+
+              'Accept');
+  next();
+});
+const tt = 'dist/ng6crud/index.html/'
+express(() => {
+  console.log('testing',`${tt}`);
+})
+  .use(express.static(path.join(__dirname, 'favicon.ico')))
   .use(bodyParser.urlencoded({
     extended: true
   }))
   .use(bodyParser.json({
     type: 'application/json'
   }))
-  .get('\/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '\./dist/ng6crud/index.html'));
+  .get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/ng6crud/index.html'))
+  })
+  .use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   })
   .listen(port, () => {
-    console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port} test ${tt}`);
   })
+
+module.exports = express;

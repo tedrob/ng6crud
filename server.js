65a8851f30d40d8841a63e986b4acacdc1b8bc02
Ted Robinson <tfqbtd@aol.com>
2018-06-16 01:33:49 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express'),
       path    = require('path'),
       port    = process.env.Port || 5000,
+      bodyParser  = require('body-parser'),
       pg      = require('pg');
 
 express()
-  .use(express.static(path.join(__dirname, './dist/ng6crud/')))
-  .use((req, res, next) => {
+  .use(express.static(path.join(__dirname, '\./dist/ng6crud/favicon.ico')))
+  /* .use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
-  })
-  .get('/*', (req, res) => {
+  }) */
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(bodyParser.json({
+    type: 'application/json'
+  }))
+  .get('\/*', (req, res) => {
     res.sendFile(path.join(__dirname, '\./dist/ng6crud/index.html'));
   })
   .listen(port, () => {
-    console.log(`listening on ${ port}`);
-
+    console.log(`listening on port ${port}`);
   })
